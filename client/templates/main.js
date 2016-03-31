1510b941f8c4e0477d5eec45c070abb2fb8d1346
shadowRR <contact@jeancharles-ory.com>
2016-03-31 15:57:03 +0200

    added split system

--- client/templates/main.js
+++ client/templates/main.js
@@ -1,7 +1,23 @@
 /**/
 
-Template.main.helpers({
+Template.main.helpers( {
     authenticated() {
         return !!Meteor.user();
     }
-});
+} );
+
+Template.app.onRendered( function () {
+    Split( [ '#content', '#sidebar' ], {
+        sizes: [ 75, 25 ],
+        minSize: [ 400, 200 ],
+        gutterSize: 8,
+        cursor: 'col-resize'
+    } )
+
+    Split( [ '#nav', '#pinned' ], {
+        direction: 'vertical',
+        sizes: [ 75, 25 ],
+        gutterSize: 8,
+        cursor: 'row-resize'
+    } )
+} );
